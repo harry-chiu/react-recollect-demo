0ca8fa855008f70631d9c8f4c54ff01c4c2304f5
Harry Chiu <harrychiu1106@gmail.com>
2020-04-15 16:34:37 +0800

    Updates

---
+++ precache-manifest.028283b8cfda55e823e33320b24ee528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ac17176128bd078226189625a116c3",
+    "url": "/react-recollect-demo/index.html"
+  },
+  {
+    "revision": "c5e5e96f40c5b4e23f4f",
+    "url": "/react-recollect-demo/static/css/main.b888f887.chunk.css"
+  },
+  {
+    "revision": "033ec8804f081e54262c",
+    "url": "/react-recollect-demo/static/js/2.afc5164b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-recollect-demo/static/js/2.afc5164b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e5e96f40c5b4e23f4f",
+    "url": "/react-recollect-demo/static/js/main.dcb76234.chunk.js"
+  },
+  {
+    "revision": "621a80d5d513955d0de3",
+    "url": "/react-recollect-demo/static/js/runtime-main.68e50c9a.js"
+  }
+]);
